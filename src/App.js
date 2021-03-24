3b297e8b866de7a89f2e4f272dcaf8a4c8c37792
김기홍 <68697807+KimGiHong@users.noreply.github.com>
2021-03-24 20:47:39 +0900

    Study React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom'; // 특정 주소에 특정 컴포넌트를 보여주겠다라는 것이다.
+import { Route, Link, Switch } from 'react-router-dom'; // 특정 주소에 특정 컴포넌트를 보여주겠다라는 것이다.
 import Home from './Home';
 import About from './About';
 import Profiles from './Profiles';
@@ -23,10 +23,16 @@ function App() {
         </li>
       </ul>
       <hr />
+      <Switch>
         <Route path="/" component={Home} exact/>
         <Route path="/about" component={About} />
         <Route path="/profiles" component={Profiles} />
         <Route path="/history" component={HistorySample} />
+        <Route render={({ location }) => <div>
+          <h2>이 페이지는 존재하지 않습니다.</h2>
+          <p>{location.pathname}</p>
+        </div> } />
+      </Switch>
     </div>
   );
 }
--- src/Profiles.js
+++ src/Profiles.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import Profile from './Profile';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
+import RouterHookSample from './RouterHookSample';
 
 function Profiles() {
     return(
         <div>
             <h3>사용자 목록</h3>
             <ul>
-                <li><Link to="/profiles/kimgihong">kimgihong</Link></li>
-                <li><Link to="/profiles/homer">homer</Link></li>
+                <li>
+                    <NavLink 
+                        to="/profiles/kimgihong" 
+                        activeStyle={{ background:"black", color:"white"}}
+                    >
+                        kimgihong
+                    </NavLink>
+                </li>
+                <li><NavLink to="/profiles/homer" activeStyle={{ background:"black", color:"white"}}>homer</NavLink></li>
             </ul>
             <Route path="/profiles" exact render={() => <div>사용자를 선택해주세요.</div>} />
             <Route path="/profiles/:username" component={Profile}/>
+            <RouterHookSample />
         </div>
     );
 }
